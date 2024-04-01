27ffa0244e0f27a9b896f7a19a9521807acb3c96
Sachin Singh <contactsachinsingh@gmail.com>
2024-04-01 12:37:53 +0530

    Added missing dependencies

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,4 +1,46 @@
 declare const _exports: {
+  Logger: {
+    new (
+      args?:
+        | {
+            silent?: boolean | undefined;
+            verbose?: boolean | undefined;
+            watch?: boolean | undefined;
+          }
+        | undefined
+    ): {
+      _silent: boolean;
+      _verbose: boolean;
+      _watch: boolean;
+      print(text: string, info?: string | undefined): void;
+      log(text: string, info?: string | undefined): void;
+      error(text: string, info?: string | undefined): void;
+      success(text: string, info?: string | undefined): void;
+      warn(text: string, info?: string | undefined): void;
+      muted(text: string, info?: string | undefined): void;
+      verbose(text: string): void;
+      timeStart(id: string): void;
+      timeEnd(id: string): void;
+    };
+  };
+  getLogger(args: {
+    silent?: boolean | undefined;
+    verbose?: boolean | undefined;
+    watch?: boolean | undefined;
+  }): {
+    _silent: boolean;
+    _verbose: boolean;
+    _watch: boolean;
+    print(text: string, info?: string | undefined): void;
+    log(text: string, info?: string | undefined): void;
+    error(text: string, info?: string | undefined): void;
+    success(text: string, info?: string | undefined): void;
+    warn(text: string, info?: string | undefined): void;
+    muted(text: string, info?: string | undefined): void;
+    verbose(text: string): void;
+    timeStart(id: string): void;
+    timeEnd(id: string): void;
+  };
   crossPath(path: string): string;
   fromPackage: typeof import('./lib/fromPackage').fromPackage;
   deps: typeof import('./lib/fromPackage').deps;
